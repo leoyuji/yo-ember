a438a56f9b645c174cbd17c1d5aa25cd7c860177
leoyuji <leoyujiuehara@gmail.com>
2017-04-20 12:03:00 -0300

    WIP: Add lesson3

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,8 +12,12 @@ export default Ember.Controller.extend({
 
   actions: {
    saveInvitation() {
-      alert(`Saving of the following email adress is in progress: ${this.get('emailAddress')}`);
-      this.set('responseMessage', `Thnak you! We've just saved you email adress: ${this.get('emailAddress')} `);
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', { email: email});
+      newInvitation.save();
+       
+      this.set('responseMessage', `Thank you! We've just saved you email adress: ${this.get('emailAddress')} `);
       this.set('emailAddress', '');
     }
   }
